4268c13e53f36c72ff1db29a0f3e39102ba69b90
Mateusz Zbytniewski <mateusz.zbytniewski@gmail.com>
2018-07-17 18:50:18 +0200

    Small fix
    Change redirection operator

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Add alias to .bashrc file"
-echo "alias cotylewazy='sudo ls |xargs -n1 -P$(nproc) -I{} du -sh {} 2>/dev/null'" > ~/.bashrc
+echo "alias cotylewazy='sudo ls |xargs -n1 -P$(nproc) -I{} du -sh {} 2>/dev/null'" >> ~/.bashrc
 echo "Reload bash config"
 . ~/.bashrc
